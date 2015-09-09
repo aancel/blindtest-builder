3be21ee5324783c94f1e32944ac1f8971e14d6e7
Alexandre Ancel <alexandre.ancel@cemosis.fr>
2015-09-09 21:06:33 +0200

    Update: - Removed accents from titles - Removed processing of already downloaded titles - Increased size of string to find in youtube field

--- blintest-builder.py
+++ blintest-builder.py
@@ -4,8 +4,18 @@ import csv
 import glob
 import os
 import sys
+# mandatory to handle accents
+# see https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import unicodedata
 
 # TODO check that needed executables are installed
+
+# Great code from https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string
+def remove_accents(input_str):
+    nkfd_form = unicodedata.normalize('NFKD', unicode(input_str))
+    return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
 
 def main():
 
@@ -26,7 +36,7 @@ def main():
     csvdata = csv.reader(csvfile, delimiter=',')
     for row in csvdata:
         # Find if the current row has a youtube link for a video we can download
-        if(row[4].find("youtube") != -1):
+        if(row[4].find("://www.youtube.com") != -1):
             print row
             # Create a directory for the playlist if it doesn't exist
             if( not os.path.exists(row[0]) ):
@@ -36,13 +46,20 @@ def main():
             fprefix = "./" + row[0] + "/" + row[1] + "." + row[3] + "_-_" + row[2]
             fprefix = fprefix.replace(" ", "_")
             fprefix = fprefix.replace("'", "_")
-            filename = "\"" + fprefix + ".%(ext)s\""
-            print filename
 
-            # Go into the directory of the current playlist
-            cmd = "youtube-dl --restrict-filenames -o " + filename + " -x \"" + row[4] + "\""
-            print cmd
-            os.system(cmd)
+            fprefix = remove_accents(fprefix)
+
+            f = glob.glob(fprefix + ".*")
+            if(len(f) > 0):
+                print "The file for " + f[0] + " already exists. Skipping"
+            else:
+                filename = "\"" + fprefix + ".%(ext)s\""
+                print filename
+
+                # Go into the directory of the current playlist
+                cmd = "youtube-dl --restrict-filenames -o " + filename + " -x \"" + row[4] + "\""
+                print cmd
+                os.system(cmd)
 
             # Add the mp3 to the playlist
             # find it back with the correct extension
